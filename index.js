ee8a37613088705f1c5a7c6c4296190eda8bad03
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2020-10-23 22:59:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,38 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
+const badWords = [
+    'nigga',
+    'nggr',
+    'nigger',
+    'fuck',
+    'shit',
+    'asshole',
+    'cunt',
+    'fag',
+    'fagg',
+    'fuk',
+    'fck',
+    'fcuk',
+    'assfuck',
+    'assfucker',
+    'fucker',
+    'dick',
+    'cum',
+    'penis',
+    'dildo',
+    'vegina',
+    'condom',
+    'condum',
+    'lmao',
+    'lma0',
+    'lmfoa',
+    'lmfa0',
+    'pussy',
+    'af',
+    'https',
+    '.com',
+];
 
 fs.readdir("./commands/", (err, files) => {
 
@@ -30,7 +62,16 @@ bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
 
-  let prefix = ".";
+  for (i = 0; i < badWords.length; i++) {
+      var rgx = new RegExp(badWords[i], 'gi');
+      if (rgx.test(message.content)) {
+          message.delete().catch(O_o=>{});
+          message.channel.send("Message deleted to keep things family friendly for all servers! (Links are potentially harmful and are also removed.)").then(msg => msg.delete(2000));
+          return;
+      }
+  }
+
+  let prefix = "?";
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
